685d2d9b3bb8c0d899f85aa7da6219ac1f5f2ef2
sxr <sxr@data4truth.com>
2021-11-08 23:32:22 +0800

    feat(初始化)

---
+++ admin/src/test/java/com/xushifei/admin/AdminApplicationTests.java
@@ -0,0 +1,11 @@
+package com.xushifei.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
---
+++ authorization/src/test/java/com/xushifei/authorization/AuthorizationApplicationTests.java
@@ -0,0 +1,11 @@
+package com.xushifei.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}

4815f1dcf55d72a2b611021b2056ebc1a38ef531
许世飞 <xusf.data4truth.com>
2021-11-09 18:09:37 +0800

    feat(初始化)：认证服务依赖新增

---
+++ gateway/src/main/java/com/xushifei/gateway/filter/TokenFilter.java
@@ -0,0 +1,31 @@
+package com.xushifei.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * <p>
+ * 认证和授权
+ * </p>
+ *
+ * @author xushifei
+ * @date 2021/11/9
+ */
+@Slf4j
+@Component
+public class TokenFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}

afe0454ae52e5c85eb2f16ae2742fbbacd3e3809
许世飞 <xusf.data4truth.com>
2021-11-15 18:09:23 +0800

    feat(初始化)：删除认证模块，留下授权模块，开始摸索如何搭建授权服务器

---
+++ admin/src/main/java/com/xushifei/admin/config/OAuth2LoginSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.xushifei.admin.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * oauth2登录
+ *
+ * @author xushifei
+ * @date 2021/11/15
+ */
+@EnableWebSecurity
+public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+        .oauth2Login(withDefaults());
+  }
+}
--- admin/src/main/java/com/xushifei/admin/controller/HelloController.java
+++ admin/src/main/java/com/xushifei/admin/controller/HelloController.java
@@ -4,17 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * <p>
- *
- * </p>
- *
  * @author xushifei
  * @date 2021/11/9
  */
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello";
-    }
+  @GetMapping("/")
+  public String hello() {
+    return "hello";
+  }
 }

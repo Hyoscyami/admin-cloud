589f367f38a875bcd526747ab56db2d782bac706
许世飞 <xusf.data4truth.com>
2021-12-13 17:53:29 +0800

    feat(分布式id生成)：web相关提交

--- id-generate/id-generate-server/src/main/java/com/xushifei/id/generate/server/IdGenerateServerApplication.java
+++ id-generate/id-generate-server/src/main/java/com/xushifei/id/generate/server/IdGenerateServerApplication.java
@@ -1,9 +1,14 @@
 package com.xushifei.id.generate.server;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan({
+  "com.baomidou.mybatisplus.samples.quickstart.mapper",
+  "com.xushifei.id.generate.server.mapper"
+})
 public class IdGenerateServerApplication {
 
   public static void main(String[] args) {
